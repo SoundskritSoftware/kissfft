d8623918f92ef85b6ff7cab7735c3f0c111858f2
FredSoundskrit <frederic.lepoutre@soundskrit.ca>
2024-08-14 16:32:06 -0400

    adding new wrapper for more m values

--- kiss_fft.c
+++ kiss_fft.c
@@ -35,6 +35,7 @@ static void kf_bfly2(
     }while (--m);
 }
 
+
 static void kf_bfly4(
         kiss_fft_cpx * Fout,
         const size_t fstride,
@@ -82,6 +83,102 @@ static void kf_bfly4(
         ++Fout;
     }while(--k);
 }
+
+static void kf_bfly4_m16(
+    kiss_fft_cpx* Fout,
+    const size_t fstride,
+    const kiss_fft_cfg st,
+    const size_t m
+)
+{
+    kiss_fft_cpx* tw1, * tw2, * tw3;
+    kiss_fft_cpx scratch[6];
+    size_t k = m;
+    const size_t m2 = 2 * m;
+    const size_t m3 = 3 * m;
+
+
+    tw3 = tw2 = tw1 = st->twiddles;
+
+    do {
+        C_FIXDIV(*Fout, 4); C_FIXDIV(Fout[m], 4); C_FIXDIV(Fout[m2], 4); C_FIXDIV(Fout[m3], 4);
+
+        C_MUL(scratch[0], Fout[m], *tw1);
+        C_MUL(scratch[1], Fout[m2], *tw2);
+        C_MUL(scratch[2], Fout[m3], *tw3);
+
+        C_SUB(scratch[5], *Fout, scratch[1]);
+        C_ADDTO(*Fout, scratch[1]);
+        C_ADD(scratch[3], scratch[0], scratch[2]);
+        C_SUB(scratch[4], scratch[0], scratch[2]);
+        C_SUB(Fout[m2], *Fout, scratch[3]);
+        tw1 += fstride;
+        tw2 += fstride * 2;
+        tw3 += fstride * 3;
+        C_ADDTO(*Fout, scratch[3]);
+
+        if (st->inverse) {
+            Fout[m].r = scratch[5].r - scratch[4].i;
+            Fout[m].i = scratch[5].i + scratch[4].r;
+            Fout[m3].r = scratch[5].r + scratch[4].i;
+            Fout[m3].i = scratch[5].i - scratch[4].r;
+        }
+        else {
+            Fout[m].r = scratch[5].r + scratch[4].i;
+            Fout[m].i = scratch[5].i - scratch[4].r;
+            Fout[m3].r = scratch[5].r - scratch[4].i;
+            Fout[m3].i = scratch[5].i + scratch[4].r;
+        }
+        ++Fout;
+    } while (--k);
+}
+
+
+static void kf_bfly4_m1(
+    kiss_fft_cpx* Fout,
+    const size_t fstride,
+    const kiss_fft_cfg st
+)
+{
+    kiss_fft_cpx* tw1, * tw2, * tw3;
+    kiss_fft_cpx scratch[6];
+
+    tw3 = tw2 = tw1 = st->twiddles;
+
+
+        C_FIXDIV(*Fout, 4); C_FIXDIV(Fout[1], 4); C_FIXDIV(Fout[2], 4); C_FIXDIV(Fout[3], 4);
+
+        C_MUL(scratch[0], Fout[1], *tw1);
+        C_MUL(scratch[1], Fout[2], *tw2);
+        C_MUL(scratch[2], Fout[3], *tw3);
+
+        C_SUB(scratch[5], *Fout, scratch[1]);
+        C_ADDTO(*Fout, scratch[1]);
+        C_ADD(scratch[3], scratch[0], scratch[2]);
+        C_SUB(scratch[4], scratch[0], scratch[2]);
+        C_SUB(Fout[2], *Fout, scratch[3]);
+
+        // tw1 += fstride;
+        // tw2 += fstride * 2;
+        // tw3 += fstride * 3;
+
+        C_ADDTO(*Fout, scratch[3]);
+
+        if (st->inverse) {
+            Fout[1].r = scratch[5].r - scratch[4].i;
+            Fout[1].i = scratch[5].i + scratch[4].r;
+            Fout[3].r = scratch[5].r + scratch[4].i;
+            Fout[3].i = scratch[5].i - scratch[4].r;
+        }
+        else {
+            Fout[1].r = scratch[5].r + scratch[4].i;
+            Fout[1].i = scratch[5].i - scratch[4].r;
+            Fout[3].r = scratch[5].r - scratch[4].i;
+            Fout[3].i = scratch[5].i + scratch[4].r;
+        }
+
+}
+
 
 /*
 static void kf_bfly4_m4(
@@ -1029,6 +1126,14 @@ void kf_work(
                 // kf_bfly4_m4_remapped(fast_Fout, fstride, st);
                 // f_map_fast_Fout_to_Fout(fast_Fout, Fout);
                
+            }
+            else if (m == 1)
+            {
+                kf_bfly4_m1(Fout, fstride, st);
+            }
+            else if (m == 16) 
+            {
+                kf_bfly4_m16(Fout, fstride, st, m);
             }
             else
             {
